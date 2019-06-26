21413c21b66cf2825269957b613d6de5079acef9
birromer <hummescmpa@gmail.com>
2019-06-26 10:27:58 -0300

    updated emacs config

---
+++ .emacs.d/elpa/counsel-org-capture-string-20180816.724/counsel-org-capture-string-autoloads.el
@@ -0,0 +1,28 @@
+;;; counsel-org-capture-string-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "counsel-org-capture-string" "counsel-org-capture-string.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from counsel-org-capture-string.el
+
+(autoload 'counsel-org-capture-string "counsel-org-capture-string" "\
+Supply input to `org-capture-string' from counsel.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "counsel-org-capture-string" '("counsel-org-capture-string-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; counsel-org-capture-string-autoloads.el ends here
---
+++ .emacs.d/elpa/counsel-org-capture-string-20180816.724/counsel-org-capture-string-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel-org-capture-string" "20180816.724" "Counsel for org-capture-string" '((emacs "25.1") (ivy "0.10")) :commit "0fd5d72397a9268a89dd26de2a6c355f127453ac" :keywords '("outlines") :authors '(("Akira Komamura" . "akira.komamura@gmail.com")) :maintainer '("Akira Komamura" . "akira.komamura@gmail.com") :url "https://github.com/akirak/counsel-org-capture-string")
