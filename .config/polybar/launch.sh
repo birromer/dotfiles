b1b59f3f5917e5e29cb9899fd56060e3541a3f37
birromer <hummescmpa@gmail.com>
2020-03-08 12:58:52 -0300

    added new polybar theme

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,16 +1,12 @@
-#!/bin/bash
 #!/usr/bin/env sh
+
+## Add this to your wm startup file.
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-#polybar example &
-
-for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar example -c ~/.config/polybar/config & done
-feh --bg-scale ~/.config/wall.png
-
-echo "Bars launched..."
+polybar -c ~/.config/polybar/config.ini main &
