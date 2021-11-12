66b1214528356d334c52156dd24375e77d43819e
Bernardo Hummes <hummescmpa@gmail.com>
2021-11-12 18:52:58 +0100

    added new scripts for xrandr external screen

---
+++ .screenlayout/27.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI1 --off --output VIRTUAL1 --primary --mode VIRTUAL1.446-1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL2 --off
---
+++ .screenlayout/ultrawide-usb+wide+hdmi.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --mode 1920x1080 --pos 2240x1080 --rotate normal --output DP1 --primary --mode $1 --pos 1920x0 --rotate normal --output HDMI1 --off --output VIRTUAL1 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL2 --off
