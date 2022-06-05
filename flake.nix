50297d712b07435ec4b32d0fa00de83523857acb
Bernardo Hummes <hummesbe@gmail.com>
2022-06-05 14:03:46 +0200

    corrected home manager import and usage

--- flake.nix
+++ flake.nix
@@ -12,22 +12,49 @@
   };
 
   # packages that will be installed
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, home-manager }:
 
     let
       system = "x86_64-linux";
+      user = "birromer";
+      host = "s51";
+
+      lib = nixpkgs.lib;
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
       };
-      lib = nixpkgs.lib;
+
     in {
       nixosConfigurations = {
-        birromer = lib.nixosSystem {
+        s51 = lib.nixosSystem {
           inherit system;
-          modules = [ ./hosts/s51/configurations.nix ];
+          modules = [
+            ./hosts/s51/configurations.nix
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPacakges = true;
+              home-manager.users.birromer = {
+                imports = [ ./home.nix ];
+              };
+            }
+          ];
         };
       };
+
+      # hmConfig = {
+      #   s51 = home-manager.lib.homeManagerConfiguration {
+      #     inherit system pkgs;
+      #     username = "${user}";
+      #     homeDirectory = "/home/${user}";
+      #     configuration = {
+      #       imports = [
+      #         ./hosts/s51/home.nix
+      #       ];
+      #     };
+      #   };
+      # };
+
     };
 
 }
