004d15765e8abd8bd07ad0b4e8b86ba4f115d694
Bernardo Hummes <hummesbe@gmail.com>
2022-06-05 13:41:14 +0200

    added home-manager source

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
   # dependencies and whatever is needed
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   # packages that will be installed
