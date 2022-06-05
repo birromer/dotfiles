ee88a363582c39d191abd8c6752929b10baf0b34
Bernardo Hummes <hummesbe@gmail.com>
2022-06-05 19:43:53 +0200

    updated doom flake

--- modules/editors/emacs/default.nix
+++ modules/editors/emacs/default.nix
@@ -1,30 +1,19 @@
-{ config, pkgs, ... }:
+{ config, pkgs, home-manager, ... }:
 
 {
-  services.emacs.enable = true;
-
-  system.userActivationScripts = {                    # Installation script every time nixos-rebuild is run. So not during initial install.
-    doomEmacs = {
-      text = ''
-        source ${config.system.build.setEnvironment}
-        DOOM="$HOME/.emacs.d"
-        if [ ! -d "$DOOM" ]; then
-          git clone https://github.com/hlissner/doom-emacs.git $DOOM
-          yes | $DOOM/bin/doom install
-          rm -r $HOME/.doom.d
-          ln -s ./doom.d $HOME/.doom.d
-          $DOOM/bin/doom sync
-        else
-          $DOOM/bin/doom sync
-        fi
-      '';
-    };
+  home.file.".doom.d" = {                       # Get Doom Emacs
+    source = ./doom.d;                          # Sets up symlink name ".doom.d" for file "doom.d"
+    recursive = true;                           # Allow symlinking a directory
+    onChange = builtins.readFile ./doom.sh;     # If an edit is detected, it will run this script. Pretty much the same as what is now in default.nix but actually stating the terminal and adding the disown flag to it won't time out
   };
 
-  environment.systemPackages = with pkgs; [
+  programs = {
+    emacs.enable = true;                        # Get Emacs
+  };
+
+  home.packages = with pkgs; [
     ripgrep
     coreutils
     fd
   ];
-
 }
