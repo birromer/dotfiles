a27dbad8232b9f5e52baf4e70d8dcfdf32a5b17a
Bernardo Hummes <hummesbe@gmail.com>
2023-01-27 11:22:49 +0100

    creating new flake for mbp

--- flake.nix
+++ flake.nix
@@ -1,35 +1,7 @@
+{ config, lib, pkgs, ... }:
+
 {
-  description = "NixOS System Configuration";
-
-  # dependencies and whatever is needed
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
-
-  # packages that will be installed
-  outputs = inputs @ { self, nixpkgs, home-manager }:
-
-    let
-      system = "x86_64-linux";
-      user = "birromer";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      lib = nixpkgs.lib;
-
-    in {
-      nixosConfigurations = (
-        import ./hosts {
-          inherit (nixpkgs) lib;
-          inherit inputs user system home-manager;
-        }
-      );
-    };
-
 }
